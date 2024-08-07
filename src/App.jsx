97993db90daee72a84d684120dc654984a2ce9ea
Aswin-04 <e.aswin004@gmail.com>
2024-08-07 15:26:47 +0530

    Working Particles.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ParticlesApp from './components/ParticlesApp'  
+
+function App() {
+
+  return (
+    <div>
+     <ParticlesApp />
+    </div>
+  )
+}
+
+export default App
